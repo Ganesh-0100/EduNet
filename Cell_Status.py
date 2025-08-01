0b85416e3d0377646525a3f6420715a2d297c2c7
Ganesh-0100 <vishwakarmaganesh8320@gmail.com>
2025-08-01 10:46:50 +0530

    Add files via upload

---
+++ Cell_Status.py
@@ -0,0 +1,29 @@
+power_dict = {}
+
+for i in range(3):
+    key = input("Enter the value of the quantities (i + 1): ")
+    value = input("Enter value for {key}: ")
+    power_dict[key] = value
+
+print("Your dictionary is:")
+print(power_dict)
+
+power_dict = {}
+
+for i in range(3):
+    key = input("Enter the value of the quantities (i + 1): ")
+    value = input("Enter value for {key}: ")
+    power_dict[key] = value
+
+print("Your dictionary is:")
+print(power_dict)
+
+power_dict = {}
+
+for i in range(3):
+    key = input("Enter the value of the quantities (i + 1): ")
+    value = input("Enter value for {key}: ")
+    power_dict[key] = value
+
+print("Your dictionary is:")
+print(power_dict)
